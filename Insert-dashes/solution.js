9110e30c1d0418c7fc4cf533a259c9979e961961
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-10 19:07:46 -0800

    updating psuedo code

---
+++ Insert-dashes/solution.js
@@ -0,0 +1,25 @@
+//ToDo insert - between 2 odd numbers
+
+
+//find a method that will locate 2 odd numbers together.
+    //eval?  parseInt? 
+    //separate the the values and .push into an array
+    //make interger into a string
+    //split the string
+    //.push into a array
+
+
+function insertDash(num) {
+    var string = num.toString()
+    var separatedString = string.split()
+    console.log('separatedString', separatedString);
+    for (var i = 0;  i < separatedString.length; i += 1) {   
+   
+}
+
+insertDash(454793);
+
+//concatenate  the "-" between those 2 odd numbers in this string
+
+
+//return the concatenated string
