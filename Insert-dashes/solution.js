97d065e89c5936aaba11e97418eb2c468a7e4ab8
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-10 19:38:53 -0800

    updated logic

--- Insert-dashes/solution.js
+++ Insert-dashes/solution.js
@@ -4,22 +4,30 @@
 //find a method that will locate 2 odd numbers together.
     //eval?  parseInt? 
     //separate the the values and .push into an array
-    //make interger into a string
+    //make integer into a string
     //split the string
     //.push into a array
+    //run a loop to inspect the logic
+    //push the logic into an array to evaluate the conditions in which I will add the dash into the array
+    //insert the - with splice at a certain index in the separatedString array.
 
 
-function insertDash(num) {
-    var string = num.toString()
-    var separatedString = string.split()
-    console.log('separatedString', separatedString);
-    for (var i = 0;  i < separatedString.length; i += 1) {   
-   
-}
+    function insertDash(num) {
+        var string = num.toString();
+        var separatedString = string.split("");
+        
+        var logicCheck = []
+        
+        for(i=0; i<separatedString.length; i++) {
+            var separatedNumber = separatedString[i];
+          if (separatedNumber%2 == 0) {
+              console.log(true);
+            logicCheck.push('true');
+            } else {
+            console.log(false);
+            logicCheck.push('false');
+            }
+        }; 
+        console.log(logicCheck);
+    };
 
-insertDash(454793);
-
-//concatenate  the "-" between those 2 odd numbers in this string
-
-
-//return the concatenated string
