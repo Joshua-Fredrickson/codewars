a90c00831253e2fc83ae2403c3b2f95a0dbc8144
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-16 19:07:24 -0800

    not a finished solution

---
+++ nice_array/solution.js
@@ -0,0 +1,18 @@
+function isNice(arr){
+    let answer = true; //establish the true answer
+    
+    if (arr.length === 0) {
+      return false;
+    }
+    
+    for(i = 0; i < arr.length; ++i) {   //iterate through the array
+      let arrayValue = arr[i];
+      console.log('array value', arr[i]);
+      if(arr.includes(arrayValue-1) || arr.includes(arrayValue+1)) {  //eval the logic
+        answer = true;
+      } else {
+        answer = false;
+      }
+     } 
+      return answer
+    }
