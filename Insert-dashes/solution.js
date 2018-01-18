2c2432e777cac2b00ca33ab820e05eb0abec8b3c
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-18 12:44:13 -0800

    finals

--- Insert-dashes/solution.js
+++ Insert-dashes/solution.js
@@ -1,17 +1,3 @@
-//ToDo insert - between 2 odd numbers
-
-
-//find a method that will locate 2 odd numbers together.
-    //eval?  parseInt? 
-    //separate the the values and .push into an array
-    //make integer into a string
-    //split the string
-    //.push into a array
-    //run a loop to inspect the logic
-    //push the logic into an array to evaluate the conditions in which I will add the dash into the array
-    //insert the - with splice at a certain index in the separatedString array.
-
-
     function insertDash(num) {
         var string = num.toString();
         var separatedString = string.split("");
