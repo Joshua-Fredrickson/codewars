0dbcf47d23546e4b7b450c07ef886886e1535148
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-24 00:23:19 -0800

    final

--- Insert-dashes/solution.js
+++ Insert-dashes/solution.js
@@ -1,33 +1,17 @@
-//ToDo insert - between 2 odd numbers
-
-
-//find a method that will locate 2 odd numbers together.
-    //eval?  parseInt? 
-    //separate the the values and .push into an array
-    //make integer into a string
-    //split the string
-    //.push into a array
-    //run a loop to inspect the logic
-    //push the logic into an array to evaluate the conditions in which I will add the dash into the array
-    //insert the - with splice at a certain index in the separatedString array.
-
-
-    function insertDash(num) {
-        var string = num.toString();
-        var separatedString = string.split("");
-        
-        var logicCheck = []
-        
-        for(i=0; i<separatedString.length; i++) {
-            var separatedNumber = separatedString[i];
-          if (separatedNumber%2 == 0) {
-              console.log(true);
-            logicCheck.push('true');
+function insertDash(num) { 
+    var prev=''; 
+    var newstring='';
+        num = num.toString();
+        for (var i=0; i<num.length; i++){
+            parseInt(num[i])%2 == 0 ? current='even' : current='odd';
+            if (current=='odd' && prev=='odd'){
+                newstring=newstring + '-' + num[i];
+                prev='odd';
             } else {
-            console.log(false);
-            logicCheck.push('false');
+                newstring=newstring + num[i];
+                 prev=current;
             }
-        }; 
-        console.log(logicCheck);
+        }
+      return newstring;
     };
 
