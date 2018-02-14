6fed0343598bb6d347ab7e63931e2b136fa80598
Joshua Fredrickson <uafredrickson@gmail.com>
2018-02-14 10:33:23 -0800

    round 2 and still no final solution

--- chords/solution.js
+++ chords/solution.js
@@ -15,3 +15,55 @@ function chords(root) {
 
 
 //this is not a 100% working solution.  The issue is when the note index is greater than 5, the array is not long enough and produces "undefined".  I need to refactor to possibly creating an "if/else" statements.
+
+
+
+//round 2  -- still not working!  issue with my Boolean logic
+
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+let majorChord = [];
+let minorChord = [];
+
+
+function chords(root) {
+  majorChord.push(notes[notes.indexOf(root)]);
+  
+  if((notes.indexOf(root) + 4) != undefined) {
+    majorChord.push(notes[(notes.indexOf(root) + 4)]);
+  } else {
+    majorChord.push(notes[(notes.indexOf(root) - 8)]);
+  };
+  
+  if((notes.indexOf(root) + 7) != undefined) {
+    majorChord.push(notes[(notes.indexOf(root) + 7)]);
+  } else {
+    majorChord.push(notes[(notes.indexOf(root) - 5)]);
+  };
+  
+   minorChord.push(notes[notes.indexOf(root)]);
+  
+  if((notes.indexOf(root) + 4) != undefined) {
+    minorChord.push(notes[(notes.indexOf(root) + 3)]);
+  } else {
+    minorChord.push(notes[(notes.indexOf(root) - 9)]);
+  };
+  
+  if((notes.indexOf(root) + 7) != undefined) {
+    minorChord.push(notes[(notes.indexOf(root) + 7)]);
+  } else {
+    minorChord.push(notes[(notes.indexOf(root) - 5)]);
+  };
+  
+ 
+ console.log(majorChord);
+ console.log(minorChord);
+ 
+  return [majorChord, minorChord];
+
+majorChord.splice(0, majorChord.length)
+minorChord.splice(0, minorChord.length)
+ 
+console.log("clear", majorChord);
+console.log("clear", minorChord);
+}
