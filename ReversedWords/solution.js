1e2c1e8cdd95fe70f71b817ba74c0ee82cee1f55
Joshua Fredrickson <uafredrickson@gmail.com>
2018-03-14 11:06:18 -0700

    not my solution but sooooo elegant

--- ReversedWords/solution.js
+++ ReversedWords/solution.js
@@ -1,18 +1,18 @@
 //This is not the solution!!!!!
-
-
+// function reverseWords(str){
+//   //split the words into an array
+//   let wordArray = str.split(' ');
+//   console.log(wordArray);
+//   //reverse the order of the array
+//   let reversedArray = wordArray.reverse();
+//   console.log(reversedArray);
+//   //turn back into string
+//   let reversedString = reversedArray.toString();
+//   //remove ,  from reversedString using replace and regex
+//   let ftw = reversedString.replace(/,/g, ' ');
+//   return ftw;
+//   }
 
 function reverseWords(str){
-  //split the words into an array
-  let wordArray = str.split(' ');
-  console.log(wordArray);
-  //reverse the order of the array
-  let reversedArray = wordArray.reverse();
-  console.log(reversedArray);
-  //turn back into string
-  let reversedString = reversedArray.toString();
-  //remove ,  from reversedString using replace and regex
-  let ftw = reversedString.replace(/,/g, ' ');
-  return ftw;
-  }
-
+  return str.split('').reverse().join(' ');
+}
